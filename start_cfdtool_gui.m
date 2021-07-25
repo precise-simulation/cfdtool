097378eb2e8c52912782e666cdec452a73802802
Precise Simulation <18078865+precise-simulation@users.noreply.github.com>
2021-07-25 10:04:20 +0200

    CFDTool Version 1.7.2 Build 21.07.206

---
+++ start_cfdtool_gui.m
@@ -0,0 +1,10 @@
+function start_cfdtool_gui(varargin)
+%START_CFDTOOL_GUI Start the CFDTool GUI
+%
+%   This command starts the main application GUI. CFDTool can
+%   optionally also be launched with the test, teste, and testt
+%   arguments as
+%
+%       cfdtool test     % Run all test suites
+
+% Copyright 2013-2020 Precise Simulation, Ltd.
