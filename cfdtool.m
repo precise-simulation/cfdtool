33eaf999c66549a41da166b05d7261964b2baf86
Precise Simulation <18078865+precsim@users.noreply.github.com>
2025-08-05 10:35:31 +0700

    CFDTool

---
+++ cfdtool.m
@@ -0,0 +1,24 @@
+%Start CFDTool.
+%
+%   This command starts the main application GUI. The toolbox can
+%
+%
+%       cfdtool test                   % Run all test suites
+%       cfdtool testt                  % Run tests for GUI tutorials
+%
+%
+%   and optionally with a vector specifying which tests to run
+%
+%       cfdtool test/testt [#]         % Run selected tests for test suite
+%
+%   To automatically load a model file or (.fes) script on startup use
+%
+%       cfdtool filename               % Start GUI and load model
+%
+%   Furthermore, the following maintenance tasks can also be performed
+%
+%       cfdtool activate               % Activate product license
+%       cfdtool deactivate             % Release/free activated license
+%       cfdtool sysinfo                % Print system information
+
+% Copyright 2013-2025 Precise Simulation, Ltd.
